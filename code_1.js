ff0c193b1b4dcae8919379bfba074397e327be55
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-14 22:19:35 +0500

    Add files via upload

---
+++ code_1.js
@@ -0,0 +1,4 @@
+let famous_movie = 'Kaalia';
+let message = '"Jahan teri ye coding hai, Meri jaa mujhe error khabar hai, Bach na saka koi,error kitne, Lambe hain mere error itne, Dekh idhar yaar Ameen Sir kidhar hai"';
+console.log(`${famous_movie} composing song, ${message}`);
+export {};
---
+++ code_1.ts
@@ -0,0 +1,3 @@
+let famous_movie :string = 'Kaalia';
+let message :string = '"Jahan teri ye coding hai, Meri jaa mujhe error khabar hai, Bach na saka koi,error kitne, Lambe hain mere error itne, Dekh idhar yaar Ameen Sir kidhar hai"';
+console.log(`${famous_movie} composing song, ${message}`);
