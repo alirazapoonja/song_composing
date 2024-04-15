a99591171b95ab18059130b590a017cf25bf583e
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-15 23:27:51 +0500

    Delete code_1.js

--- code_1.js
+++
@@ -1,4 +0,0 @@
-let famous_movie = 'Kaalia';
-let message = '"Jahan teri ye coding hai, Meri jaa mujhe error khabar hai, Bach na saka koi,error kitne, Lambe hain mere error itne, Dekh idhar yaar Ameen Sir kidhar hai"';
-console.log(`${famous_movie} composing song, ${message}`);
-export {};
