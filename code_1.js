117cf42358a323a3561715ca535db7b237d35daf
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-04-15 23:31:36 +0500

    famous movie song in typescript

---
+++ code_1.js
@@ -0,0 +1,4 @@
+let famous_movie = 'Kaalia';
+let message = '"\n Jahan teri ye coding hai,\n Meri jaa mujhe error khabar hai,\n Bach na saka koi error kitne,\n Lambe hain mere error itne,\n Dekh idhar yaar Ameen Sir Aidhar hai.';
+console.log(`${famous_movie} composing song, ${message}`);
+export {};
---
+++ code_1.ts
@@ -0,0 +1,3 @@
+let famous_movie :string = 'Kaalia';
+let message :string = '"\n Jahan teri ye coding hai,\n Meri jaa mujhe error khabar hai,\n Bach na saka koi error kitne,\n Lambe hain mere error itne,\n Dekh idhar yaar Ameen Sir Aidhar hai.';
+console.log(`${famous_movie} composing song, ${message}`);
