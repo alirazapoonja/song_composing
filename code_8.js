f768f1a83615dbb4c3638c29303d775147ae69bc
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-05-19 00:06:41 +0500

    Add files via upload

---
+++ code_8.js
@@ -0,0 +1,5 @@
+"use strict";
+let primitivetypes = ["boolean", "number", "string"];
+for (let i = 3; i < primitivetypes.length; i++) {
+    console.log(primitivetypes[i]);
+}
---
+++ code_8.ts
@@ -0,0 +1,10 @@
+let primitivetypes: string[] = ["boolean", "number", "string"];
+for (let i = 3; i < primitivetypes.length; i++) {
+    console.log(primitivetypes[i]);
+}
+
+
+
+
+
+
