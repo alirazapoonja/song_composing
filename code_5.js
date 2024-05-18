3d646f917d2bb500fe3a246d6d0697270d27f055
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-05-18 23:28:00 +0500

    Add files via upload

---
+++ code_5.js
@@ -0,0 +1,4 @@
+"use strict";
+let myName = "\t\n typescipt ke sau coding karke humne dekhe hain, \t\n \n Ek mein bhi error the (sau mein bhi error hain)"; // This saves the name with whitespace
+console.log(myName); //Shows the name with whitespace
+console.log(myName.trim()); // Shows the name without whitespace
