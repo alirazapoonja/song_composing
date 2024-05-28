4f16e1eec42a13162168c127f9725ffc9767cf25
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-05-28 20:16:43 +0500

    Add files via upload

---
+++ code_12.js
@@ -0,0 +1,10 @@
+"use strict";
+let guests = ["Asim Azhar", "sahir ali bagga,", "ali zafar,"];
+console.log("A new song  yaariyan ehd e wafa!");
+// Adding more guests
+guests.unshift("aima baig");
+guests.splice(guests.length / 2, 0, "Ahad Raza");
+guests.push("Osman Khalid");
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, Haan yaad hai wo dosti ka har maza?`);
+});
