acdd151af5d8da7d40c50327ad1427f5cd8ddbad
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-07 22:39:35 +0500

    Delete code_13.js

--- code_13.js
+++
@@ -1,7 +0,0 @@
-import chalk from "chalk";
-let songfrommovie = "Movie Name Border";
-let songinmessage = "\n error aate hain, hamein tadpaate hain\n Jo coding aati hai woh compile ho jaati hai\n Ke resut kab aaoge, ke result kab aaoge\n Likho result kab aaoge\n Ke tum bin yeh project soona soona hai.";
-let songinmessage2 = "\n Kisi discord waali ne, kisi group wali ne \n Hamein error likha hai Yeh humse poochha hai\n \n Kisi ki laptop ne, kisi ki mobile ne \n Kisi ki phone ne, kisi ke facebook ne \n Kisi ke linkedin ne, kisi ke discord ne\n  \n mehanti larko ne mehanti larkiyo ne \n programing raaton mein, adhoori coding ne \n Aur poochha hai tarsi nigaahon ne \n Ke resut kab aaoge, ke result kab aaoge \n Likho result kab aaoge \n Ke tum bin yeh project soona soona hai.";
-console.log(chalk.red.bold(songfrommovie));
-console.log(chalk.green(songinmessage));
-console.log(chalk.blue(songinmessage2));
