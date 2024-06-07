05a3df98d656f7b0598b09c976f3cd83d4d34d87
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-07 22:44:07 +0500

    Delete code_7.ts

--- code_7.ts
+++
@@ -1,9 +0,0 @@
-let myfavouritesong: string = "Lahoo Ke Do Rang";
-let mysonglyrics: string = "typescript main Aate Hai Kya Kya error, teacher hi Jaane To Hum Kaise Jaane.";
-console.log(`movie name ${myfavouritesong} composing song,  ${mysonglyrics}`);
-
-
-
-
-
-
