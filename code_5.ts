f3a63fe03c264f26e1fa303dc85cf4d805a5d8d5
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-07 22:43:12 +0500

    Delete code_5.ts

--- code_5.ts
+++
@@ -1,5 +0,0 @@
-let myName: string =
-  "\t\n typescipt ke sau coding karke humne dekhe hain, \t\n \n Ek mein bhi error the (sau mein bhi error hain)"; // This saves the name with whitespace
-
-console.log(myName); //Shows the name with whitespace
-console.log(myName.trim()); // Shows the name without whitespace
