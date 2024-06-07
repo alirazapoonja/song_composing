0217b1ef4e4a93c453efc8e9e817782130169858
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-07 22:42:49 +0500

    Delete code_5.js

--- code_5.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let myName = "\t\n typescipt ke sau coding karke humne dekhe hain, \t\n \n Ek mein bhi error the (sau mein bhi error hain)"; // This saves the name with whitespace
-console.log(myName); //Shows the name with whitespace
-console.log(myName.trim()); // Shows the name without whitespace
