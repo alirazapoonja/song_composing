a4b166d0da97a5fa56e819c2e5effc07e97e4738
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 11:02:56 +0500

    song composing specific structure, template literals for string interpolation, and multiline strings.
    Object Declaration and Initialization:
    Logging Object Properties with Template Literals:
    Multiline String Declaration:
    Logging the Multiline String:

---
+++ code_12.js
@@ -0,0 +1,9 @@
+"use strict";
+let song = {
+    movie: "Jo jeeta wohi sikandar",
+    song: "Pehla Nasha Pehla khumar ",
+    year: 1992
+};
+console.log(`movie name ${song.movie} song ${song.song}, movie published in ${song.year}`);
+let songmessage = "\n Chaahe tum kuchh na kaho maine code kar liya \n Ki course programing ka typescript ko chun liya \n Chun liya... maine Code liya. \n \n Pehla code, pehla program \n Naya code hai naya intezar \n Kar loon main loop ka istamal \n Aye typescript Tu hi bata \n \n coding hi karta hon in typescript mein kahin \n Yaa main bhool jaaun inn error mein kahin \n Ikk kar doon variable aur string \n Kaho sir kya karoon kya nahin \n \n Usne coding ki Kuch aise dhang se \n result aa gaya Woh compile karna se \n Reh jaao jaise main Haar ke \n Aur coding use kare if else se";
+console.log(songmessage);
---
+++ code_12.ts
@@ -0,0 +1,14 @@
+let song: { movie: string; song: string; year: number } = {
+  movie: "Jo jeeta wohi sikandar",
+  song: "Pehla Nasha Pehla khumar ",
+  year: 1992,
+};
+
+console.log(
+  `movie name ${song.movie} song ${song.song}, movie published in ${song.year}`
+);
+
+let songmessage: string =
+  "\n Chaahe tum kuchh na kaho maine code kar liya \n Ki course programing ka typescript ko chun liya \n Chun liya... maine Code liya. \n \n Pehla code, pehla program \n Naya code hai naya intezar \n Kar loon main loop ka istamal \n Aye typescript Tu hi bata \n \n coding hi karta hon in typescript mein kahin \n Yaa main bhool jaaun inn error mein kahin \n Ikk kar doon variable aur string \n Kaho sir kya karoon kya nahin \n \n Usne coding ki Kuch aise dhang se \n result aa gaya Woh compile karna se \n Reh jaao jaise main Haar ke \n Aur coding use kare if else se";
+
+console.log(songmessage);
