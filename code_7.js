3f065fdece9688029751681367bcb3b3799bb98b
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 10:29:39 +0500

    song composing with using backtick 2
    song composing with using backtick 2 and print movie name  and movie song lyrics.

---
+++ code_7.js
@@ -0,0 +1,4 @@
+"use strict";
+let myfavouritesong = "Lahoo Ke Do Rang";
+let mysonglyrics = "typescript main Aate Hai Kya Kya error, teacher hi Jaane To Hum Kaise Jaane.";
+console.log(`movie name ${myfavouritesong} composing song,  ${mysonglyrics}`);
---
+++ code_7.ts
@@ -0,0 +1,9 @@
+let myfavouritesong: string = "Lahoo Ke Do Rang";
+let mysonglyrics: string = "typescript main Aate Hai Kya Kya error, teacher hi Jaane To Hum Kaise Jaane.";
+console.log(`movie name ${myfavouritesong} composing song,  ${mysonglyrics}`);
+
+
+
+
+
+
