5524e6fc2cd444ae2286cf7a583d73ac7d42c1af
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 11:12:57 +0500

    song composing with using array method
    Array Declaration and Initialization:
    Adding Elements to the Array:
    Iterating Over the Array with forEach:
    Array Manipulation: Uses unshift to add an element to the beginning of the array, splice to add an element at a specific position, and push to add an element to the end of the array.
    Iteration with forEach: Uses the forEach method to iterate over the array.
    Template Literals: Uses template literals to format and log the output.

---
+++ code_14.js
@@ -0,0 +1,9 @@
+let guests = ["Ameen Sir", "Mubasir Sir", "Amjad Sir"];
+// Adding more guests
+guests.unshift("Zia Sir");
+guests.splice(guests.length / 3, 0, "Daniel Sir");
+guests.push("Imran Sir");
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, Main Koi Aisa typescript per Geet Gaoon?`);
+});
+export {};
