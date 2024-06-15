ee965b8d9e92382ac6a9aa878be8da0b497516fd
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 10:48:38 +0500

    song composing Array of Strings, Iteration with forEach, Logging with Template Literals
    song composing Array of Strings, Iteration with forEach, Logging with Template Literals
    Variable Declaration with Type Annotation
    Array forEach Method:
    Arrow Function:
    Template Literals:

---
+++ code_10.ts
@@ -0,0 +1,11 @@
+let transports: string[] = ["error aya", "result na aya", "Samajh na aave"];
+
+transports.forEach(transport => {
+    console.log(`kuch kuch hota hai ${transport}.`);
+});
+
+
+
+
+
+
