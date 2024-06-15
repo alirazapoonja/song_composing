de529d2870f86bff0d62d4f5def9a591999b4211
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 10:27:13 +0500

    song composing with using backtick
    song composing with using backtick in message

---
+++ code_6.ts
@@ -0,0 +1,3 @@
+let favoritesong =
+  "typescipt Ka Funda,\n Aao sikhaun tumhe typescript ka funda,\n Yeh nahi pyare koi maamuli program,\n Is mein chupa hai javascript ka falsafa,"; // This saves your favorite number
+console.log(`My favorite song is ${favoritesong}`); // This shows the message with your favorite number
