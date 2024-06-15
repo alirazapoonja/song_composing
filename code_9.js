07e2db4d50a5b4f21ae039ee74b52dd989665b01
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-15 10:43:52 +0500

    song composing with type annotations, array handling, loops, and template literals.
    This type of Typescript code is basic and demonstrates fundamental concepts such as type annotations, array handling, loops, and template literals.
    1) Variable Declaration with Type Annotation:
    2) For-Of Loop:
    3) Template Literals: using backticks (`)

---
+++ code_9.js
@@ -0,0 +1,5 @@
+"use strict";
+let songs = ["Chal", "Saare program Ki command",];
+for (let song of songs) {
+    console.log(`${song}, coding coding coding`);
+}
---
+++ code_9.ts
@@ -0,0 +1,9 @@
+let songs: string[] = ["Chal", "Saare program Ki command", ];
+for (let song of songs ) {
+    console.log(`${song}, coding coding coding`);
+}
+
+
+
+
+
