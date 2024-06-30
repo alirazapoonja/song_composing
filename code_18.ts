1b351ae0072cb604ffa7e97887c332a493426194
Ali Raza <160298055+alirazapoonja@users.noreply.github.com>
2024-06-30 22:45:11 +0500

    Jaane kyun log typescript sika karta hai
    Jaane kyun log typescript sika karta hai  song composing with using import chalk

---
+++ code_18.ts
@@ -0,0 +1,17 @@
+import chalk, { Chalk } from "chalk";
+
+let songfrommovie: string = "Mehboob Ki Mehandi";
+
+let songinmessage: string =
+  "\n Jaane kyon log Typescript sika karate hain \n Jaane kyon log Typescript sika karate hain\n sikhana ke badale sikhaya karate hain \n Jaane kyon log Typescript sika karate hain.";
+  
+let songinmessage2: string =
+  "\n slides milati hai,  coding nahin milati \n Raahein sikhnay mein Kabhi manjil nahin milati \n Dil tut jaata hai, Naakaam hota hai \n na sikhnay mein logon ka Yahi anjaam hota hai\n Koi kya jaane, Kyon ye paravaane,\n Kyon machalate hain, siknay mein jalate hain \n slide dekh dekh ke Divaane coding karate hain \n slide dekh dekh ke Divaane coding karate hain \n Jaane kyon log Typescript sika karate hain";
+
+  let songinmessage3: string =
+  "\n  june mein student ko,exam ki date rulaati hai \n facebook mein jab student ko daniyal sir ki yaad aati hai \n Ye jindagi yuun hi Barabaad hoti hai \n Har vaqt comments main pe sir Koi fariyaad hoti hai\n Na comments ka naam chalata hai, Na duaaon se kaam chalata hai\n Jahar ye phir bhi arguments Kyon karatein hain \n Jahar ye phir bhi arguments Kyon karatein hain \n Jaane kyon log Typescript sika karate hain";
+
+console.log(chalk.red.bold(songfrommovie));
+console.log(chalk.green(songinmessage));
+console.log(chalk.blue(songinmessage2));
+console.log(chalk.cyan(songinmessage3));
